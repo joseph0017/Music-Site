b77d34aa3ec8a2249effcb689a813275fc5c62d1
joseph0017 <josephogiku96@gmail.com>
2022-12-05 14:11:00 -0800

    fixed music player

--- mysite/api/models.py
+++ mysite/api/models.py
@@ -5,20 +5,20 @@ from django.utils import timezone
 
 
 class Music_Player(models.Model):
-    music = models.FileField(upload_to='music/')
-    album_cover = models.FileField(upload_to='album_cover/')
+    musicSrc = models.FileField(upload_to='music/')
+    cover = models.FileField(upload_to='album_cover/')
     pause_song = models.BooleanField(null=False, default=False)
     skip_song = models.BooleanField(null=False, default=False)
     fast_forward = models.BooleanField(null=False, default=False)
     rewind = models.BooleanField(null=False, default=False)
     current_song = models.CharField(max_length=100, null=True)
     duration = models.CharField(max_length=20, default=False)
-    title = models.CharField(max_length=100, null=True)
-    text = models.TextField(max_length=1000, null=True)
+    name = models.CharField(max_length=100, null=True)
+    singer = models.TextField(max_length=1000, null=True)
     created_date = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
-        return self.title
+        return self.name
 
     class Meta:
         verbose_name = 'music'
