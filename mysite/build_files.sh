93ac2d209629a036b253dc419e70e82494d5cb42
joseph0017 <josephogiku96@gmail.com>
2022-12-21 04:59:34 -0800

    deploying through railway

--- mysite/build_files.sh
+++
@@ -1,6 +0,0 @@
-# build_files.sh
-
-echo " BUILD START"
-pip install -r requirements.txt
-python3.9 manage.py collectstatic
-echo " BUILD END"
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '$ci4t(*3#+snc6j6k(_w^idq3q6_8&6f0dc0_qhc%91ybx)0nj'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -131,7 +131,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
